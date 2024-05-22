c16ccdf872d0e1f0867164b90a7583c9fafdc6c1
Mukesh Soni <imukeshsoniii@gmail.com>
2024-05-22 12:14:47 +0530

    added pages to enable routing - added about, contact and projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import { Home } from './Pages';
+import { Home, About, Projects, Contact } from './Pages';
 
 const App = () => {
     return (
@@ -9,6 +9,9 @@ const App = () => {
                 <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/projects' element={<Projects />}></Route>
+                    <Route path='/contact' element={<Contact />}></Route>
                 </Routes>
             </Router>
         </main>
