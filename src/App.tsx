90469834243e6205936cc7e8db6dfa3e9789c99f
Mukesh Soni <imukeshsoniii@gmail.com>
2024-05-22 12:07:47 +0530

    created navbar with links - created multiple links and added support for the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
 import { Home } from './Pages';
 
 const App = () => {
     return (
         <main className='bg-slate-300/20 h-full'>
             <Router>
+                <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />}></Route>
                 </Routes>
