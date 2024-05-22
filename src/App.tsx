818706ad1a0fe7c843e79cfc7c92eb53a973b75c
Mukesh Soni <imukeshsoniii@gmail.com>
2024-05-22 12:05:37 +0530

    added routing in homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home } from './Pages';
 
 const App = () => {
     return (
         <main className='bg-slate-300/20 h-full'>
-            Hello World
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                </Routes>
+            </Router>
         </main>
     );
 };
