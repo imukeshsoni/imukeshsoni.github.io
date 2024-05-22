3d3f408558681b13379eeddb36b1f3ed6a2e9f63
Mukesh Soni <imukeshsoniii@gmail.com>
2024-05-22 12:19:06 +0530

    moved navbar in parent folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import { Home, About, Projects, Contact } from './Pages';
 
 const App = () => {
